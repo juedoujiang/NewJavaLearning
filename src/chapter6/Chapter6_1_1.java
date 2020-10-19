b4627121eecfb4a7d00510a79ad385394cd77394
jiang <jiang@LAPTOP-4GI9ADJK>
2020-10-19 21:57:58 +0800

    Chapter6

---
+++ src/chapter6/Chapter6_1_1.java
@@ -0,0 +1,17 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class Chapter6_1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ´óÐ¡Ð´×ª»»
+		Scanner in = new Scanner(System.in);
+		char c = 'A';
+		char d = (char)(c+'a'-'A');
+		System.out.println(d);
+		in.close();
+	}
+
+}
---
+++ src/chapter6/Chapter6_1_2.java
@@ -0,0 +1,15 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class Chapter6_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("abc\td");
+		System.out.println("a\td");
+		in.close();
+	}
+
+}
